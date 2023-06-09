072becc5028592d5945480d2af615644b7871867
Rizky Adi <72495089+adirizq@users.noreply.github.com>
2023-06-09 23:30:17 +0700

    Update main.py

--- Generation Model Deployment/main.py
+++ Generation Model Deployment/main.py
@@ -10,33 +10,37 @@ predictor = Predict()
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        image = request.files.get("image")
+        category = request.form.get("category")
+
+        if image is None or image.filename == "" or category is None or category == "":
+            return jsonify({"error":"parameters not complete"})
+
+        try:
+            image_bytes = image.read()
+            pillow_image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+            generated_text = predictor.predict(pillow_image, category)
+            predicted_title, predicted_descripton = predictor.predict_improvement(generated_text)
+            data = {"title": predicted_title, "description": predicted_descripton}
+
+            return jsonify(data)
+    
+        except Exception as e:
+            print(e)
+            return jsonify({"error": str(e)})
+
     return "All Good"
+
 
 @app.route("/prepare", methods=["GET"])
 def prepare():
     download_models()
     return "OK"
-
-@app.route("/predict", methods=["POST"])
-def predict_image():
-    image = request.files.get("image")
-    category = request.form.get("category")
-    
-    try:
-        image_bytes = image.read()
-        pillow_image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-
-        generated_text = predictor.predict(pillow_image, category)
-        predicted_title, predicted_descripton = predictor.predict_improvement(generated_text)
-        data = {"title": predicted_title, "description": predicted_descripton}
-
-        return jsonify(data)
-    
-    except Exception as e:
-        print(e)
-        return jsonify({"error": str(e)})
     
 
 if __name__ == "__main__":
