61ea2182c07c371359d51d7eb46e0330a33228ab
adirizq <rizkyzadadi@gmail.com>
2023-06-15 14:59:56 +0700

    Migrate to EdgeGPT

--- Generation Model Deployment/test/test.py
+++ Generation Model Deployment/test/test.py
@@ -2,4 +2,6 @@ import requests
 
 response = requests.post("http://localhost:5000/predict", files={'image': open('test/console.jpeg', 'rb')}, data={'category': 'elektronik'})
 
-print(response.json())
+response = requests.post("http://localhost:5000/", files={'image': open('test/washing machine.jpeg', 'rb')})
+
+print(response.text)
--- Generation Model Deployment/utils/predict.py
+++ Generation Model Deployment/utils/predict.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 
 from transformers import AutoImageProcessor, AutoTokenizer, TFVisionEncoderDecoderModel
 from utils.chat_completion import ChatCompletion
+from EdgeGPT.EdgeUtils import Query, Cookie
 
 class Predict:
 
@@ -78,13 +79,15 @@ class Predict:
 
     def predict_improvement(self, generated_title):
 
-        title_prompt = f'Fix this product title and make it compelling and eye catching "{generated_title}". Only answer with the title in Bahasa Indonesia'
-        predicted_title = self.get_result(title_prompt)
+        title_prompt = f'Fix this product title and make it compelling and eye catching "{generated_title}". Only reply with the title in Bahasa Indonesia, do not add any explanation'
+        # predicted_title = self.get_result(title_prompt)
+        predicted_title = Query(title_prompt)
 
         description_prompt = f'Write a description for product titled "{predicted_title}", make it three sentences long. Only answer with the description in Bahasa Indonesia'
-        predicted_description = self.get_result(description_prompt)
+        # predicted_description = self.get_result(description_prompt)
+        predicted_description = Query(description_prompt)
 
-        return predicted_title, predicted_description
+        return predicted_title.output, predicted_description.output
     
      
     def get_result(self, prompt):
